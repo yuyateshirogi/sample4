80a9b273cd770e983e371a8de884301adc11b24e
手代木悠也 <teshirogiyuya@teshirogiyuunarinoMacBook-puro.local>
2018-02-22 14:07:31 +0800

    {WIP] index 完成

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
 	echo 'Hello World';
 	var_dump(1);
+
+	$name = 'yuyateshirogi'
  ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ 	<h1>Hllo Github!</h1>
+ </body>
+ </html>
